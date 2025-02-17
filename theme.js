ae63026fd8053cc500ecc11437950e345ad2d6c9
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-02-18 04:13:01 +0800

    fix: 跟进原生 isLocalPath 函数修改

--- theme.js
+++ theme.js
@@ -149,11 +149,30 @@
         retryIntervalId = setInterval(findTargetNodes, retryInterval);
     })();
 
+    const isWindows = () => {
+        return navigator.platform.toUpperCase().indexOf("WIN") > -1;
+    };
+
     const isLocalPath = (link) => {
-        // 参考原生 isLocalPath 函数
-        link = link?.trim();
-        if (!link || link.length === 0) return false;
-        return /^assets\/|file:\/\/|\\\\|[A-Z]:$/i.test(link);
+        if (!link) {
+            return false;
+        }
+
+        link = link.trim();
+        if (1 > link.length) {
+            return false;
+        }
+
+        link = link.toLowerCase();
+        if (link.startsWith("assets/") || link.startsWith("file://") || link.startsWith("\\\\") /* Windows 网络共享路径 */) {
+            return true;
+        }
+
+        if (isWindows()) {
+            const colonIdx = link.indexOf(":");
+            return 1 === colonIdx; // 冒号前面只有一个字符认为是 Windows 盘符而不是网络协议
+        }
+        return link.startsWith("/");
     };
 
     // 给 tooltip 元素添加 data-whisper-tooltip 属性值
