1d62ba1bc53f88259ef1aa81bf2cdd8cb0e6d338
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-06-22 09:13:26 +0800

    build(builder): ESLint 移动到 builder 文件夹，移除 oxlint

--- builder/eslint.config.mjs
+++ builder/eslint.config.mjs
@@ -3,21 +3,20 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
-
 export default defineConfig([
   {
-    ignores: ['tools/**', 'theme.js'],
+    ignores: ['tools/**', 'theme.js', 'dist/**', 'node_modules/**'],
   },
   { 
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], 
+    files: ['**/*.{js,mjs,cjs}'], 
     plugins: { js }, 
     extends: ['js/recommended'], 
     languageOptions: { globals: globals.browser }
   },
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
       'quotes': [2, 'single', { 'avoidEscape': true }]
     }
-  }
+  },
 ]);
